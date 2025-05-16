64b02d00675660e0542196f2f01dfe96da9add1f
Prnvpk <129866450+Prnvpk@users.noreply.github.com>
2025-05-17 03:47:12 +0530

    Add files via upload

---
+++ Calculator/javascript.js
@@ -0,0 +1,11 @@
+function buttonclick(val){
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function clearDisplay(val){
+    document.getElementById("screen").value=""
+}
+function equalclick(val){
+    var text=document.getElementById("screen").value;
+   var result=eval(text)
+   document.getElementById("screen").value=result
+}
